8e74a50720ac5f2e95e27d8bad8d390326a868ed
Kirill <18kirilllarin@gmail.com>
2022-07-22 21:24:18 +0700

    Update task2.py

---
+++ Глава 3/task2.py
@@ -0,0 +1,14 @@
+import random
+
+orel = 0
+reshka = 0
+input("Давайте подбросим монетку 100 раз, а я скажу, сколько раз выпал орел, а сколько решка. ")
+
+for i in range(100):
+    n = random.randint(1, 2)
+    if n == 1:
+        orel += 1
+    else:
+        reshka += 1
+
+print(f"Орел выпал {orel} раз, а решка {reshka} раз.")
